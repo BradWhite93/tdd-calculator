3505958d46a891ee56834781cab45d8f9165b44e
Bradley White <bradley.white@ao.com>
2022-12-30 13:38:35 +0000

    Removed redundant if statement on the char being populated

--- tdd-calculator/tdd-calculator/Calculator.cs
+++ tdd-calculator/tdd-calculator/Calculator.cs
@@ -5,18 +5,14 @@
         public virtual int Add(string numbers)
         {
             var delimiters = new List<char> { ',', '\n' };
-            var customDelimiter = '\0';
+            
             if (numbers.Contains("//"))
             {
-                customDelimiter = numbers.Split("//").Last().ToCharArray()[0];
+                var customDelimiter = numbers.Split("//").Last().ToCharArray()[0];
+                delimiters.Add(customDelimiter);
                 numbers = numbers.Remove(0, numbers.IndexOf(customDelimiter, StringComparison.Ordinal));
             }
 
-            if (customDelimiter != '\0')
-            {
-                delimiters.Add(customDelimiter);
-            }
-            
             var listOfNumbers = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
             return listOfNumbers.Sum(int.Parse);
         }
